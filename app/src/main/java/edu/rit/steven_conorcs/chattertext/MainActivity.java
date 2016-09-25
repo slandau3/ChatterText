fbf2c635248737091adff9b70e76c4c26c189086
Steven Landau <skl8881@g.rit.edu>
2016-09-24 22:31:25 -0400

    Requested permission to send and recieve sms

--- app/src/main/java/edu/rit/steven_conorcs/chattertext/MainActivity.java
+++ app/src/main/java/edu/rit/steven_conorcs/chattertext/MainActivity.java
@@ -1,5 +1,8 @@
 package edu.rit.steven_conorcs.chattertext;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -12,8 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1); // Request permission to send sms messages
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 1); // Request permission to receive sms
+
         Button b1 = (Button) findViewById(R.id.test_button); // creates the main button we are going to use
-        // SMS send stuff. Implement in a seperate function later
+
 
     }
 
