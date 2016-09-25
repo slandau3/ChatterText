acb61286de63f0df82de3b0e9a1c1b948033a32e
Steven Landau <skl8881@g.rit.edu>
2016-09-24 22:15:18 -0400

    Added SMS manager and button ID to MainActivity

--- app/src/main/java/edu/rit/steven_conorcs/chattertext/MainActivity.java
+++ app/src/main/java/edu/rit/steven_conorcs/chattertext/MainActivity.java
@@ -2,6 +2,8 @@ package edu.rit.steven_conorcs.chattertext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = (Button) findViewById(R.id.test_button); // creates the main button we are going to use
+        // SMS send stuff. Implement in a seperate function later
+        SmsManager sms = SmsManager.getDefault();
     }
 }
