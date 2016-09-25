535dcbba56ba18122f2794e335822dcfede48bdb
Steven Landau <skl8881@g.rit.edu>
2016-09-24 21:57:25 -0400

    Initial commit

---
+++ app/src/androidTest/java/edu/rit/steven_conorcs/chattertext/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.rit.steven_conorcs.chattertext;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/edu/rit/steven_conorcs/chattertext/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.rit.steven_conorcs.chattertext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
